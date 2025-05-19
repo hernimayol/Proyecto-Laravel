907e31520d6ab81f7a34f1cfafd83df2ca1379b7
Hernan <jhernanmt@gmail.com>
2025-05-19 10:30:44 -0300

    Cambios en el modelo Provincia

--- app/Models/Provincia.php
+++ app/Models/Provincia.php
@@ -3,25 +3,12 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
-<<<<<<< HEAD
-=======
 use Illuminate\Database\Eloquent\SoftDeletes;
->>>>>>> 667f010 (Configuracion del modelo y migracion de Provincias)
 
 class Provincia extends Model
 {
     use SoftDeletes;
 
-<<<<<<< HEAD
-    //
-        @var bool
-    
-    protected $primaryKey ='id';
-
-        @var string
-    
-    protected $table ='provincias'
-=======
     /**
      * Indicates if the model's ID is auto-incrementing.
      *
@@ -42,14 +29,9 @@ class Provincia extends Model
      * @var string
      */
     protected $table = 'provincias';
->>>>>>> 667f010 (Configuracion del modelo y migracion de Provincias)
 
     protected $fillable = [
         'nombre',
         'codigo'
     ];
-<<<<<<< HEAD
-
-=======
->>>>>>> 667f010 (Configuracion del modelo y migracion de Provincias)
 }
